63f8729c3c6fdec6f84a6a2bf68ddd2a105b3506
buttonpushertv <ben@buttonpusher.tv>
2019-07-24 16:21:24 -0400

    Update keymap.c
    This is a non-standard layout for a specfic application. It requires the 2nd Keyboard AutoHotKey project from here:  https://github.com/TaranVH/2nd-keyboard

--- keyboards/40percentclub/gherkin/keymaps/buttonpusher-2ndkb/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/buttonpusher-2ndkb/keymap.c
@@ -2,25 +2,50 @@
 //// Taran's github: https://github.com/TaranVH/2nd-keyboard
 //
 //Taran's project made use of a HASU USB-to-USB converter. I wondered if it was possible to
-//flash any QMK-capable keyboard with the same code. It's a default Gherkin layout with the
-//F24-wrapping code at the tail end.
+//flash any QMK-capable keyboard with the same code. It's a customized layout for a specific application with the
+//F24-wrapping code at the tail end. The non-standard layout will be heavily altered in AutoHotKey.
 
 #include QMK_KEYBOARD_H
 
-#define FN1_DEL LT(1, KC_BSPC)
+enum gherkin_layers {
+  _DEF,
+  _LWR,
+  _UPR,
+  _ADJ
+};
+
+enum gherkin_keycodes {
+  DEF = SAFE_RANGE,
+  LWR,
+  UPR,
+  ADJ
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [0] = LAYOUT_ortho_3x10(
-    KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,   KC_J,
-    KC_K,    KC_L,    KC_M,    KC_N,    KC_O,    KC_P,    KC_Q,    KC_R,    KC_S,   KC_T,
-    KC_U,    KC_V,    KC_W,    FN1_DEL, KC_X,    KC_Y,    KC_Z,    KC_1,    KC_2,   KC_3
+  [_DEF] = LAYOUT_ortho_3x10(
+    KC_A,    KC_B,    KC_C,    KC_D,     KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+    KC_K,    LWR,     KC_L,    KC_M,     KC_N,    KC_O,    KC_P,    KC_Q,    UPR,     KC_R,
+    KC_S,    KC_T,    KC_U,    KC_V,     KC_W,    KC_X,    KC_Y,    KC_Z,    KC_DOT,  KC_COMM
   ),
 
-  [1] = LAYOUT_ortho_3x10(
-    _______, _______, KC_4,    KC_5,    KC_6,   _______, _______, _______, _______, _______,
-    _______, _______, KC_7,    KC_8,    KC_9,   _______, _______, _______, _______, BL_INC,
-    _______, _______, KC_0,    _______, KC_DOT, _______, RESET,   _______, _______, BL_DEC
+  [_LWR] = LAYOUT_ortho_3x10(
+
+    KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,    _______, _______, _______,
+    KC_8,    _______, KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_SCLN, _______, _______, _______,
+    KC_LBRC, KC_RBRC, KC_QUOT, KC_GRV,   KC_SLSH, KC_SPC,  KC_BSPC, _______, _______, _______
+  ),
+
+  [_UPR] = LAYOUT_ortho_3x10(
+     _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,
+     _______, _______, _______, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_F13,
+     _______, _______, _______, KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20
+  ),
+
+  [_ADJ] = LAYOUT_ortho_3x10(
+     _______, _______, _______, _______, _______, _______, _______, _______, _______, BL_INC,
+     _______, _______, _______, _______, _______, _______, _______, _______, _______, BL_DEC,
+     _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET
   ),
 
 };
@@ -44,6 +69,26 @@ void led_set_user(uint8_t usb_led) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint8_t f24_tracker;
     switch (keycode) {
+      case LWR:
+        if (record->event.pressed) {
+          layer_on(_LWR);
+          update_tri_layer(_LWR, _UPR, _ADJ);
+        } else {
+          layer_off(_LWR);
+          update_tri_layer(_LWR, _UPR, _ADJ);
+        }
+        return false;
+        break;
+      case UPR:
+        if (record->event.pressed) {
+          layer_on(_UPR);
+          update_tri_layer(_LWR, _UPR, _ADJ);
+        } else {
+          layer_off(_UPR);
+          update_tri_layer(_LWR, _UPR, _ADJ);
+        }
+        return false;
+        break;
     case KC_A ... KC_F23: //notice how it skips over F24
     //case KC_EXECUTE ... KC_EXSEL: //exsel is the last one before the modifier keys
         if (record->event.pressed) {
